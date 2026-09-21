dc65b5b860fea600f753572e09358cc189045b5c
codingguy1026 <hanhee20111026@gmail.com>
2026-09-21 12:51:11 +0900

    chore: remove shadowed Next.js config

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  allowedDevOrigins: ['tw72j8-3000.csb.app'],
-};
-
-export default nextConfig;
